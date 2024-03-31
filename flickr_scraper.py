ae90fa441f8ea907bb81ac0f05294638b9316691
astromodder <22250203+astromodder@users.noreply.github.com>
2024-03-30 20:17:48 -0400

    fix ctrl+c, add or searches

--- flickr_scraper.py
+++ flickr_scraper.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import signal
+import sys
+import threading
 import time
 from dotenv import load_dotenv
 
@@ -12,7 +15,22 @@ load_dotenv()
 key = os.getenv('FLICKR_API_KEY', '')
 secret = os.getenv('FLICKR_API_SECRET', '')
 
+# Global flag to indicate if Ctrl+C was pressed
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+    print("\nProgram interrupted by user. Exiting...")
+
+signal.signal(signal.SIGINT, signal_handler)
+
 def get_urls(search="honeybees on flowers", n=10, download=False):
+    # Add variations to the search term
+    search_terms = [search, search.replace(" ", "_"), search.replace(" ", "-")]
+    search = " OR ".join(search_terms)
+
+    print(f'Searching for "{search}"')  # Debug command
     t = time.time()
     flickr = FlickrAPI(key, secret)
     license = ()  # https://www.flickr.com/services/api/explore/?method=flickr.photos.licenses.getInfo
@@ -31,6 +49,10 @@ def get_urls(search="honeybees on flowers", n=10, download=False):
 
     urls = []
     for i, photo in enumerate(photos):
+        if interrupted:
+            print("Interrupted by user. Exiting...")
+            sys.exit(0)
+
         if i <= n:
             try:
                 # construct url https://www.flickr.com/services/api/misc.urls.html
@@ -48,9 +70,6 @@ def get_urls(search="honeybees on flowers", n=10, download=False):
                 print("%g/%g error..." % (i, n))
 
         else:
-            # import pandas as pd
-            # urls = pd.Series(urls)
-            # urls.to_csv(search + "_urls.csv")
             print("Done. (%.1fs)" % (time.time() - t) + ("\nAll images saved to %s" % dir if download else ""))
             break
 
@@ -61,17 +80,16 @@ class PreserveQuotesAction(argparse.Action):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--search", nargs="+", default="honeybees on flowers", help="flickr search term")
-    # parser.add_argument('--search', type=str, default='honeybees on flowers', help='flickr search term')
     parser.add_argument("--n", type=int, default=10, help="number of images")
     parser.add_argument("--download", action="store_true", help="download images")
     opt = parser.parse_args()
 
     print(f"nargs {opt.search}")
-    # Check key
     help_url = "https://www.flickr.com/services/apps/create/apply"
     assert key and secret, f"Flickr API key required in flickr_scraper.py L11-12. To apply visit {help_url}"
 
     for search in opt.search:
+        print(f'Searching for "{search}"')  # Debug command
         get_urls(
             search=search,  # search term
             n=opt.n,  # max number of images
